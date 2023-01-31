997c3294ada698690e77ad545c2c09f51ca2a6ec
sahil <sahil.soni_int@acko.tech>
2023-01-31 10:45:08 +0530

    sorting done by id desc

--- src/main/java/com/example/ItService/repository/RequestRepository.java
+++ src/main/java/com/example/ItService/repository/RequestRepository.java
@@ -1,6 +1,7 @@
 package com.example.ItService.repository;
 
 import com.example.ItService.model.Request;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request,Long> {
-    List<Request> findByIsDeleted(boolean deleted);
+    List<Request> findByIsDeleted(boolean deleted, Sort sort);
 }
